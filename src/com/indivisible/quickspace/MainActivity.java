f977ff8a623123c6066bc75f58c777c4c3ffa242
indivisible-irl <github@indivisibleindustries.com>
2013-08-22 18:01:22 +0100

    Modify activity layout row layout

--- src/com/indivisible/quickspace/MainActivity.java
+++ src/com/indivisible/quickspace/MainActivity.java
@@ -1,4 +1,5 @@
 package com.indivisible.quickspace;
+
 import android.app.*;
 import android.os.*;
 import android.content.*;
@@ -8,8 +9,13 @@ public class MainActivity extends Activity
     @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(android.R.drawable.btn_plus);
+		
+		// dialog activity
+//		setContentView(android.R.drawable.btn_plus);
+		
+		// background Toast service
 		startService(new Intent(this, MainService.class));
+		
 		finish();
 	}
 }
